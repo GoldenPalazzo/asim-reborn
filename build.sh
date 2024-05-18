b21756d3fd6c3a9a3d3104d6f8b46b5cbcae6d70
Francesco Palazzo <francesco@palazzo.me>
2024-05-18 10:46:36 +0200

    Not needed anymore, in justfile

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/sh
-#
-docker build -t 68k-env:latest .
--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-#
-docker run -ti -v $PWD:/srv/ --rm 68k-env:latest \
-    vasmm68k_mot -Faout -no-opt -o "${1%.*}.bin" $1
