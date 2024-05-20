b111c7869462e4d43bdff86e6c0e90f92234f99c
Francesco Palazzo <francesco@palazzo.me>
2024-05-20 15:39:44 +0200

    Folder independent compiler

--- src/compiler.py
+++ src/compiler.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
+import pathlib
+import platform
+import os.path
 import subprocess
+
+compiler_path = (pathlib.Path(__file__).parent.parent / "bin" / (f"vasmm68k_mot_{'win.exe' if platform.system() == 'Windows' else 'linux'}")).resolve()
+print(compiler_path)
+
+arguments = ("-Fsrec","-s37","-exec=main","-no-opt")
 
 class Compiler:
     def __init__(self):
         pass
         #subprocess.run(["just", "build"])
     def compile(self, fpath):
-        print(f"Running compile.sh {fpath}")
-        print(subprocess.run(["just", "compile", fpath]))
+        command_array = [compiler_path,
+                              *arguments,
+                              "-o",
+                              f"{os.path.splitext(fpath)[0]}.h68",
+                              fpath]
+        print(f"Running {command_array}")
+        subprocess.run(command_array)
