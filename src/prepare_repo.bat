17c9120f46905bfdd296cebcaad65e08d3cca104
Francesco Palazzo <francesco@palazzo.me>
2024-05-22 21:06:13 +0200

    Download font and bare68k for python 3.12

--- src/prepare_repo.bat
+++ src/prepare_repo.bat
@@ -1,1 +1,6 @@
-curl http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe -o bin\vasmm68k_mot_win.exe
+REM Description: Download all the necessary files to build the project
+REM Author: Francesco Palazzo
+mkdir ..\bin
+mkdir ..\res
+curl http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe -o ..\bin\vasmm68k_mot_win.exe
+curl https://www.fontsquirrel.com/fonts/download/droid-sans-mono -o ..\res\DroidSansMono.ttf
