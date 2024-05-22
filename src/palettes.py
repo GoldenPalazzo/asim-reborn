2128ad4f4be1adc3df6395895666a4bd3f4fddb1
Francesco Palazzo <francesco@palazzo.me>
2024-05-22 20:56:34 +0200

    Not using QColor, just raw int

--- src/palettes.py
+++ src/palettes.py
@@ -1,8 +1,23 @@
 from PySide6.QtGui import QColor
 
 class Palette:
-    def __init__(self, background: int, foreground: int):
-        self.background = QColor(background)
-        self.foreground = QColor(foreground)
+    def __init__(self,
+                 background: int,
+                 text: int,
+                 character: int,
+                 directive: int,
+                 opcode: int,
+                 registers: int,):
+        self.background = (background)
+        self.text= (text)
+        self.character = (character)
+        self.directive = (directive)
+        self.opcode = (opcode)
+        self.registers = (registers)
 
-monokai = Palette(0x2e2e2e, 0xd6d6d6)
+monokai = Palette(0x2e2e2e, 
+                  0xd6d6d6,
+                  0xe5b567,
+                  0xb05279,
+                  0x6c99bb,
+                  0xb4d273)
