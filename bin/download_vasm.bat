20900cfa54f2b5d003318e6a69d1fe11192c17cd
Francesco Palazzo <francesco@palazzo.me>
2024-05-22 21:00:11 +0200

    Forgor :skull:

--- bin/download_vasm.bat
+++
@@ -1,1 +0,0 @@
-curl http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe -o bin\vasmm68k_mot_win.exe
--- bin/download_vasm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-#
-wget http://francescopalazzo.net/downloadable/vasmm68k_mot_linux -O bin/vasmm68k_mot_linux
-chmod +x bin/vasmm68k_mot_linux
