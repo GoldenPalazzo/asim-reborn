9811ed0d3b78a1759562cd96598cd06d62c8de4f
Francesco Palazzo <francesco@palazzo.me>
2024-05-23 11:17:13 +0200

    Workflow for windows and linux

--- src/prepare_repo.bat
+++ src/prepare_repo.bat
@@ -1,6 +1,8 @@
 REM Description: Download all the necessary files to build the project
 REM Author: Francesco Palazzo
-mkdir ..\bin
-mkdir ..\res
-curl http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe -o ..\bin\vasmm68k_mot_win.exe
-curl https://www.fontsquirrel.com/fonts/download/droid-sans-mono -o ..\res\DroidSansMono.ttf
+mkdir bin
+mkdir res
+mkdir lib
+curl http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe -o bin\vasmm68k_mot_win.exe
+curl https://www.fontsquirrel.com/fonts/download/droid-sans-mono -o res\DroidSansMono.ttf
+curl https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-win_amd64.whl -o lib\bare68k.whl
