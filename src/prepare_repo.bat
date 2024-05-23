6dee1541ceb163d91bf34ddef96ddaa2145c6959
Francesco Palazzo <francesco@palazzo.me>
2024-05-23 11:31:39 +0200

    invalid wheel name

--- src/prepare_repo.bat
+++ src/prepare_repo.bat
@@ -5,7 +5,8 @@ mkdir res
 mkdir lib
 curl http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe -o bin\vasmm68k_mot_win.exe
 curl https://www.fontsquirrel.com/fonts/download/droid-sans-mono -o res\DroidSansMono.ttf
-curl https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-win_amd64.whl -o lib\bare68k.whl
+curl https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-win_amd64.whl -o lib\bare68k-0.1.2-cp312-cp312-win_amd64.whl 
 dir bin
 dir res
 dir lib
+set PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
--- src/prepare_repo.sh
+++ src/prepare_repo.sh
@@ -6,5 +6,5 @@ chmod +x bin/vasmm68k_mot_linux
 mkdir -p res
 wget https://www.fontsquirrel.com/fonts/download/droid-sans-mono -O res/DroidSansMono.ttf
 mkdir -p lib
-wget https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl -O lib/bare68k.whl
+wget https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl -O lib/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl 
 ls -l bin res lib
