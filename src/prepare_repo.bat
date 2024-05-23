f45b9610cbba1afd2f9d5599e89f0fc1502dc97c
Francesco Palazzo <francesco@palazzo.me>
2024-05-23 11:54:06 +0200

    added redirect to curl

--- src/prepare_repo.bat
+++ src/prepare_repo.bat
@@ -3,9 +3,9 @@ REM Author: Francesco Palazzo
 mkdir bin
 mkdir res
 mkdir lib
-curl http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe -o bin\vasmm68k_mot_win.exe
-curl https://www.fontsquirrel.com/fonts/download/droid-sans-mono -o res\DroidSansMono.ttf
-curl https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-win_amd64.whl -o lib\bare68k-0.1.2-cp312-cp312-win_amd64.whl 
+curl -L -o bin\vasmm68k_mot_win.exe http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe 
+curl -L -o res\DroidSansMono.ttf https://www.fontsquirrel.com/fonts/download/droid-sans-mono
+curl -L -o lib\bare68k-0.1.2-cp312-cp312-win_amd64.whl https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-win_amd64.whl
 dir bin
 dir res
 dir lib
