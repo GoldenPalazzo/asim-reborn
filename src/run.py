2e6b7661d7894365dc94f8d4a88a7cfeb349e60e
Francesco Palazzo <francesco@palazzo.me>
2024-05-24 16:31:51 +0200

    Memory view

--- src/run.py
+++ src/run.py
@@ -2,9 +2,10 @@
 
 import sys
 from typing import Optional, Union, Callable, List
+import PySide6
 
 from PySide6.QtWidgets import QApplication, QGridLayout, QHBoxLayout, QLabel, QMainWindow, QPushButton, QTextEdit, QFileDialog, QVBoxLayout, QWidget
-from PySide6.QtGui import QFont, QTextCursor, QAction
+from PySide6.QtGui import QFont, QTextCursor, QAction, QTextOption
 from PySide6.QtCore import Qt
 
 import m68k
@@ -29,13 +30,27 @@ class Runner(QWidget):
         frame = QVBoxLayout()
         frame.addWidget(QLabel("Debugger"))
         frame.addWidget(self.current_instruction, alignment=Qt.AlignCenter)
+        memlayout = QHBoxLayout()
         reglayout = QGridLayout()
         for i in range(8):
             reglayout.addWidget(QLabel(f"D{i}"), i, 0)
             reglayout.addWidget(QLabel(f"A{i}"), i, 2)
             reglayout.addWidget(self.dregs[i], i, 1)
             reglayout.addWidget(self.aregs[i], i, 3)
-        frame.addLayout(reglayout)
+        memlayout.addLayout(reglayout)
+        # memory view
+        self.memview = QTextEdit()
+        self.memview.setReadOnly(True)
+        self.memview.setFont(QFont("Courier", 10))
+        self.memview.setLineWrapMode(QTextEdit.NoWrap)
+        self.memview.setLineWrapColumnOrWidth(0)
+        self.memview.setWordWrapMode(QTextOption.NoWrap)
+        self.memview.setTabStopDistance(40)
+        self.memview.setTabChangesFocus(True)
+        self.memview.setAcceptRichText(False)
+        self.memview.setPlaceholderText("Memory")
+        memlayout.addWidget(self.memview)
+        frame.addLayout(memlayout)
         lastregs = QHBoxLayout()
         lastregs.addWidget(QLabel("SR"))
         lastregs.addWidget(self.sreg)
@@ -59,12 +74,22 @@ class Runner(QWidget):
 
     def update_ui(self):
         regs = self.main_cpu.get_regs()
+        pc = self.main_cpu.cpu.r_pc()
         self.current_instruction.setText(self.main_cpu.get_current_line())
         for i in range(8):
             self.dregs[i].setText(f"0x{regs[f'd{i}']:08X}")
             self.aregs[i].setText(f"0x{regs[f'a{i}']:08X}")
         self.sreg.setText(f"{self.main_cpu.cpu.r_sr():016b}")
-        self.pc.setText(f"{self.main_cpu.cpu.r_pc():08X}")
+        self.pc.setText(f"{pc:08X}")
+        self.memview.clear()
+        radius = 0x80
+        mem = self.main_cpu.get_mem(max(pc-radius//2,0), radius)
+        step = 4
+        for i in range(0, len(mem), step):
+            self.memview.append(f"0x{i+pc-radius//2:08X} {mem[i:i+step].hex()}")
+        self.memview.moveCursor(QTextCursor.Start)
+
+
 
     def step(self):
         self.main_cpu.step()
