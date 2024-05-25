a92a7db791616269ee5e1656925dbbdc86c4cc3c
Francesco Palazzo <francesco@palazzo.me>
2024-05-25 16:57:54 +0200

    Forgot setuptools

--- src/build_bare68k.sh
+++ src/build_bare68k.sh
@@ -6,6 +6,7 @@ res_dir=$prj_dir/res
 lib_dir=$prj_dir/lib
 cd $lib_dir/bare68k
 pip3 install -r requirements-dev.txt
+pip3 install setuptools
 python3 setup.py build_ext -i
 python3 setup.py sdist
 python3 setup.py bdist_wheel
