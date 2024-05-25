ca897a96a7674ffde5753419b08b87608cd39f27
Francesco Palazzo <francesco@palazzo.me>
2024-05-25 13:26:12 +0200

    It doesn't matter now from where you call the scripts

--- src/prepare_repo.bat
+++ src/prepare_repo.bat
@@ -1,13 +1,12 @@
-REM Description: Download all the necessary files to build the project
-REM Author: Francesco Palazzo
-mkdir bin
-mkdir res
-mkdir lib
-curl -L -o bin\vasmm68k_mot_win.exe http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe 
-curl -L -o res\DroidSansMono.ttf https://www.fontsquirrel.com/fonts/download/droid-sans-mono
-curl -L -o res\MonoLisa-Regular.ttf https://github.com/koprab/monalisa-font/raw/master/MonoLisa-Regular.ttf
-curl -L -o lib\bare68k-0.1.2-cp312-cp312-win_amd64.whl https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-win_amd64.whl
-dir bin
-dir res
-dir lib
+set prj_dir=%~dp0
+set bin_dir=%prj_dir%bin
+set res_dir=%prj_dir%res
+set lib_dir=%prj_dir%lib
+if not exist "%bin_dir%" mkdir "%bin_dir%"
+if not exist "%res_dir%" mkdir "%res_dir%"
+if not exist "%lib_dir%" mkdir "%lib_dir%"
+curl -L -o %bin_dir%\vasmm68k_mot_win.exe http://francescopalazzo.net/downloadable/vasmm68k_mot_win.exe 
+curl -L -o %res_dir%\DroidSansMono.ttf https://www.fontsquirrel.com/fonts/download/droid-sans-mono
+curl -L -o %res_dir%\MonoLisa-Regular.ttf https://github.com/koprab/monalisa-font/raw/master/MonoLisa-Regular.ttf
+curl -L -o %lib_dir%\bare68k-0.1.2-cp312-cp312-win_amd64.whl https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-win_amd64.whl
 set PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
--- src/prepare_repo.sh
+++ src/prepare_repo.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/sh
 #
-mkdir -p bin
-wget http://francescopalazzo.net/downloadable/vasmm68k_mot_linux -O bin/vasmm68k_mot_linux
-chmod +x bin/vasmm68k_mot_linux
-mkdir -p res
-wget https://www.fontsquirrel.com/fonts/download/droid-sans-mono -O res/DroidSansMono.ttf
-wget https://github.com/koprab/monalisa-font/raw/master/MonoLisa-Regular.ttf -O res/MonoLisa-Regular.ttf
-mkdir -p lib
-wget https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl -O lib/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl 
-ls -l bin res lib
+prj_dir=$(readlink -f $(dirname $0))
+bin_dir=$prj_dir/bin
+res_dir=$prj_dir/res
+lib_dir=$prj_dir/lib
+mkdir -p $bin_dir $res_dir $lib_dir
+wget "http://francescopalazzo.net/downloadable/vasmm68k_mot_linux" -O "$bin_dir/vasmm68k_mot_linux"
+wget "https://www.fontsquirrel.com/fonts/download/droid-sans-mono" -O "$res_dir/DroidSansMono.ttf"
+wget "https://github.com/koprab/monalisa-font/raw/master/MonoLisa-Regular.ttf" -O "$res_dir/MonoLisa-Regular.ttf"
+wget "https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl" -O "$lib_dir/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl"
+chmod +x "bin/vasmm68k_mot_linux"
