47a46fff6d6f65de4bc774575be1445a3818c807
Francesco Palazzo <francesco@palazzo.me>
2025-05-22 16:21:15 +0200

    No longer needed since bare68k is now a submodule

--- src/prepare_repo.sh
+++ src/prepare_repo.sh
@@ -3,14 +3,12 @@
 prj_dir=$(dirname $(dirname $(readlink -f "$0")))
 bin_dir=$prj_dir/bin
 res_dir=$prj_dir/res
-lib_dir=$prj_dir/lib
 # Making directories
 mkdir -p $bin_dir $res_dir $lib_dir
 # Downloading resources
 curl -Lo "$res_dir/DroidSansMono.ttf" \
     "https://www.fontsquirrel.com/fonts/download/droid-sans-mono"
 curl -Lo "$res_dir/MonoLisa-Regular.ttf" \
-    "https://github.com/koprab/monalisa-font/raw/master/MonoLisa-Regular.ttf" 
+    "https://github.com/koprab/monalisa-font/raw/master/MonoLisa-Regular.ttf"
 #curl -Lo "$lib_dir/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl" \
 #    "https://github.com/GoldenPalazzo/bare68k-python3.12/releases/download/v0.1.2-3.12/bare68k-0.1.2-cp312-cp312-linux_x86_64.whl"
-git clone https://github.com/GoldenPalazzo/bare68k-python3.12.git $lib_dir/bare68k
